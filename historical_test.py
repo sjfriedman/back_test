8b65564c2d0672e5af9f825329da62614f122bc1
Sam Friedman <samueljacobfriedman@gmail.com>
2022-10-11 02:41:44 -0400

    get the graphic for back_test fully functional

--- historical_test.py
+++ historical_test.py
@@ -12,6 +12,7 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
 
+
 # df = ['ticker']['buy_date']['sell_date']['buy_sell_time']['pct_importance']
 
 
@@ -30,12 +31,12 @@ def compare_sp(s_date, e_date):
         s_date = str(sp_data.index[0].date())
 
     sp_data = sp_data.loc[datetime.strptime(s_date, '%Y-%m-%d') : datetime.strptime(e_date, '%Y-%m-%d')]
-    sp_data['cum_percent_change'] = ((sp_data['close'])).pct_change().cumsum()
+    sp_data['cum_percent_change'] = (sp_data['close'] - sp_data['close'].iloc[0]) / sp_data['close'].iloc[0]
     sp_data = sp_data.fillna(0)
 
     sp_net = (100 * (sp_data['close'].iloc[-1] - sp_data['close'].iloc[0]) / sp_data['close'].iloc[0])
 
-    return sp_data, str(sp_net)
+    return sp_data, sp_net
 
 
 def test_strategy(df, show_stats = False):
@@ -43,24 +44,32 @@ def test_strategy(df, show_stats = False):
     all_stock = pd.DataFrame()
     for ticker in df.iterrows():
         stock_data = si.get_data(ticker[1]['ticker'], start_date = datetime.strptime(ticker[1]['buy_date'], '%Y-%m-%d'), end_date = datetime.strptime(ticker[1]['sell_date'], '%Y-%m-%d') + timedelta(days=1))
-        stock_data['percent_change'] = (stock_data[ticker[1]['buy_sell_time']]).pct_change().cumsum()
+        stock_data['sale_index'] = ticker[0]
+        stock_data['percent_change'] = (stock_data['close'] - stock_data['close'].iloc[0]) / stock_data['close'].iloc[0]
         stock_data['real_percent_change'] = stock_data['percent_change'] * ticker[1]['pct_importance']
         all_stock = all_stock.append(stock_data)
     all_stock = all_stock.fillna(0)
 
     # gets the daily growth of all stocks
+    gains_per_stock = all_stock.groupby('sale_index')['real_percent_change'].last()
     grouped = all_stock.groupby(all_stock.index)['real_percent_change'].mean()
 
-    # # gets overall growth of stratgey
-    stocks_net = reduce(lambda y, z: y + z, (map(lambda x: df.iloc[x]['pct_importance'] * (all_stock.loc[(all_stock.index == df.iloc[x]['sell_date']) & (all_stock['ticker'] == df.iloc[x]['ticker'])][df.iloc[x]['buy_sell_time']].values[0] - all_stock.loc[(all_stock.index == df.iloc[x]['buy_date']) & (all_stock['ticker'] == df.iloc[x]['ticker'])][df.iloc[x]['buy_sell_time']].values[0]) / all_stock.loc[(all_stock.index == df.iloc[x]['buy_date']) & (all_stock['ticker'] == df.iloc[x]['ticker'])][df.iloc[x]['buy_sell_time']].values[0], df.index)))
+    # gets full stock growth
+    for row in df.iterrows():
+        grouped.loc[grouped.index > row[1]['sell_date']] = grouped.loc[grouped.index > row[1]['sell_date']] + gains_per_stock.loc[gains_per_stock.index == row[0]].values
+
+    # gets overall growth of stratgey
+    stocks_net = 100 * reduce(lambda y, z: y + z, (map(lambda x: df.iloc[x]['pct_importance'] * (all_stock.loc[(all_stock.index == df.iloc[x]['sell_date']) & (all_stock['ticker'] == df.iloc[x]['ticker'])][df.iloc[x]['buy_sell_time']].values[0] - all_stock.loc[(all_stock.index == df.iloc[x]['buy_date']) & (all_stock['ticker'] == df.iloc[x]['ticker'])][df.iloc[x]['buy_sell_time']].values[0]) / all_stock.loc[(all_stock.index == df.iloc[x]['buy_date']) & (all_stock['ticker'] == df.iloc[x]['ticker'])][df.iloc[x]['buy_sell_time']].values[0], df.index)))
 
     # gets s&p data
     sp_data = compare_sp(df['buy_date'].min(), df['sell_date'].max())
 
 
-    plt.plot(sp_data[0].index, sp_data[0]['cum_percent_change'], label = 'S&P')
-    plt.plot(grouped.index, grouped.values, label = 'TICKERS')
-    plt.title("S&P500 GAIN:" + sp_data[1] + " | TICKERS GAIN: " + str(round(stocks_net, 5)) + "%")
+    plt.plot(sp_data[0].index, 100 * sp_data[0]['cum_percent_change'], label = 'S&P')
+    plt.plot(grouped.index, 100 * grouped.values, label = 'TICKERS')
+    plt.text(sp_data[0].loc[sp_data[0].index== sp_data[0].index.max()].index, 100 * sp_data[0].loc[sp_data[0].index == sp_data[0].index.max()]['cum_percent_change'], '({}, {})'.format(sp_data[0].loc[sp_data[0].index == sp_data[0].index.max()].index.date[0], round(100 * sp_data[0].loc[sp_data[0].index == sp_data[0].index.max()]['cum_percent_change'].values[0],2)))
+    plt.text(grouped.loc[grouped.index == grouped.index.max()].index, 100 * grouped.loc[grouped.index == grouped.index.max()].values, '({}, {})'.format(grouped.loc[grouped.index == grouped.index.max()].index.date[0], round(100 * grouped.loc[grouped.index == grouped.index.max()].values[0],2)))
+    plt.title("S&P500 GAIN: " + str(round(sp_data[1],2)) + "% | TICKERS GAIN: " + str(round(stocks_net, 2)) + "%")
     plt.xlabel('Date')
     plt.ylabel('Percent Change')
     plt.legend()
--- stock_to_market_compare.py
+++ stock_to_market_compare.py
@@ -22,30 +22,32 @@ def single_stock_compare_sp500(ticker, s_date = None, e_date = str(date.today())
     if not s_date:
         s_date = str(sp_data.index[0].date())
 
+
     # Gets stock data
     stock_data = si.get_data(ticker, start_date = datetime.strptime(s_date, '%Y-%m-%d'), end_date = datetime.strptime(e_date, '%Y-%m-%d'))
-    stock_data['cum_percent_change'] = (stock_data['close']).pct_change().cumsum()
+    stock_data['cum_percent_change'] = (stock_data['close'] - stock_data['close'].iloc[0]) / stock_data['close'].iloc[0]
     stock_data = stock_data.fillna(0)
+
+    # print(stock_data.to_csv())
 
     # Makes sure to get S&P data for same dates as stock
     s_date = str(stock_data.index[0].date())
 
     # Gets S&P data for datetime and increment
     sp_data = sp_data.loc[datetime.strptime(s_date, '%Y-%m-%d') : datetime.strptime(e_date, '%Y-%m-%d')]
-    sp_data['cum_percent_change'] = ((sp_data['close'])).pct_change().cumsum()
+    sp_data['cum_percent_change'] = (sp_data['close'] - sp_data['close'].iloc[0]) / sp_data['close'].iloc[0]
     sp_data = sp_data.fillna(0)
 
     # calculates total percent change of S&P and Stock
     sp_net = (100 * (sp_data['close'].iloc[-1] - sp_data['close'].iloc[0]) / sp_data['close'].iloc[0])
     stock_net = (100 * (stock_data['close'].iloc[-1] - stock_data['close'].iloc[0]) / stock_data['close'].iloc[0])
 
-
     # Plots data points
-    plt.plot(sp_data.index,sp_data['cum_percent_change'], label = 'S&P')
-    plt.plot(stock_data.index, stock_data['cum_percent_change'], label = 'ticker'.upper())
+    plt.plot(sp_data.index,100 * sp_data['cum_percent_change'], label = 'S&P')
+    plt.plot(stock_data.index, 100 * stock_data['cum_percent_change'], label = 'ticker'.upper())
     plt.title("S&P GAIN: " + str(round(sp_net, 2)) + "% | " + ticker.upper() + " GAIN: " + str(round(stock_net, 2)) + "%")
-    plt.text(sp_data.loc[sp_data.index == sp_data.index.max()].index, sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'], '({}, {})'.format(sp_data.loc[sp_data.index == sp_data.index.max()].index.date[0], round(sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'].values[0],2)))
-    plt.text(stock_data.loc[stock_data.index == stock_data.index.max()].index, stock_data.loc[stock_data.index == stock_data.index.max()]['cum_percent_change'], '({}, {})'.format(stock_data.loc[stock_data.index == stock_data.index.max()].index.date[0], round(stock_data.loc[stock_data.index == stock_data.index.max()]['cum_percent_change'].values[0],2)))
+    plt.text(sp_data.loc[sp_data.index == sp_data.index.max()].index, 100 * sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'], '({}, {})'.format(sp_data.loc[sp_data.index == sp_data.index.max()].index.date[0], round(100 * sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'].values[0],2)))
+    plt.text(stock_data.loc[stock_data.index == stock_data.index.max()].index, 100 * stock_data.loc[stock_data.index == stock_data.index.max()]['cum_percent_change'], '({}, {})'.format(stock_data.loc[stock_data.index == stock_data.index.max()].index.date[0], round(100 * stock_data.loc[stock_data.index == stock_data.index.max()]['cum_percent_change'].values[0],2)))
     plt.xlabel('Date')
     plt.ylabel('Percent Change')
     plt.legend()
@@ -65,22 +67,22 @@ def multi_stock_compare_sp500(tickers, s_date, e_date = str(date.today())):
     all_stock = pd.DataFrame()
     for ticker in tickers:
         stock_data = si.get_data(ticker, start_date = datetime.strptime(s_date, '%Y-%m-%d'), end_date = datetime.strptime(e_date, '%Y-%m-%d'))
-        stock_data['cum_percent_change'] = (stock_data['close']).pct_change().cumsum()
+        stock_data['cum_percent_change'] = (stock_data['close'] - stock_data['close'].iloc[0]) / stock_data['close'].iloc[0]
         all_stock = all_stock.append(stock_data)
     grouped = all_stock.groupby(all_stock.index)['cum_percent_change'].mean()
     grouped = grouped.fillna(0)
 
     # Gets S&P data for datetime and increment
     sp_data = sp_data.loc[datetime.strptime(s_date, '%Y-%m-%d') : datetime.strptime(e_date, '%Y-%m-%d')]
-    sp_data['cum_percent_change'] = ((sp_data['close'])).pct_change().cumsum()
+    sp_data['cum_percent_change'] = (sp_data['close'] - sp_data['close'].iloc[0]) / sp_data['close'].iloc[0]
 
     sp_net = (100 * (sp_data['close'].iloc[-1] - sp_data['close'].iloc[0]) / sp_data['close'].iloc[0])
     stocks_net = 100 * reduce(lambda y, z: y + z, (map(lambda x: (all_stock.loc[(all_stock.index == all_stock.index.max()) & (all_stock['ticker'] == x.upper())]['close'].values[0] - all_stock.loc[(all_stock.index == all_stock.index.min()) & (all_stock['ticker'] == x.upper())]['close'].values[0]) / all_stock.loc[(all_stock.index == all_stock.index.min()) & (all_stock['ticker'] == x.upper())]['close'].values[0], tickers))) / len(tickers)
 
-    plt.plot(sp_data.index,sp_data['cum_percent_change'], label = 'S&P')
-    plt.plot(grouped.index, grouped.values, label = 'TICKERS')
-    plt.text(sp_data.loc[sp_data.index == sp_data.index.max()].index, sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'], '({}, {})'.format(sp_data.loc[sp_data.index == sp_data.index.max()].index.date[0], round(sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'].values[0],2)))
-    plt.text(grouped.loc[grouped.index == grouped.index.max()].index, grouped.loc[grouped.index == grouped.index.max()].values, '({}, {})'.format(grouped.loc[grouped.index == grouped.index.max()].index.date[0], round(grouped.loc[grouped.index == grouped.index.max()].values[0],2)))
+    plt.plot(sp_data.index,100 * sp_data['cum_percent_change'], label = 'S&P')
+    plt.plot(grouped.index, 100 * grouped.values, label = 'TICKERS')
+    plt.text(sp_data.loc[sp_data.index == sp_data.index.max()].index, 100 * sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'], '({}, {})'.format(sp_data.loc[sp_data.index == sp_data.index.max()].index.date[0], round(100 * sp_data.loc[sp_data.index == sp_data.index.max()]['cum_percent_change'].values[0],2)))
+    plt.text(grouped.loc[grouped.index == grouped.index.max()].index, 100 * grouped.loc[grouped.index == grouped.index.max()].values, '({}, {})'.format(grouped.loc[grouped.index == grouped.index.max()].index.date[0], round(100 * grouped.loc[grouped.index == grouped.index.max()].values[0],2)))
     plt.title("S&P GAIN: " + str(round(sp_net, 2)) + "% | TICKERS GAIN: " + str(round(stocks_net, 2)) + "%")
     plt.xlabel('Date')
     plt.ylabel('Percent Change')
@@ -89,8 +91,8 @@ def multi_stock_compare_sp500(tickers, s_date, e_date = str(date.today())):
 
 if __name__ == "__main__":
     single_stock_compare_sp500('aapl', '2022-01-04', '2022-10-6')
-    # single_stock_compare_sp500('aapl')
-    # print(multi_stock_compare_sp500(['aapl', 'msft', 'tsla', 'abnb'], '2022-01-04', '2022-10-06'))
+    single_stock_compare_sp500('aapl')
+    multi_stock_compare_sp500(['aapl', 'msft', 'tsla', 'abnb'], '2022-01-04', '2022-10-06')
 
 
 #
